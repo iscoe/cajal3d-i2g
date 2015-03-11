d33667e3ff9ce82b45899d402cedd75302e80b77
mjpekala <mike.pekala@gmail.com>
2015-03-11 09:19:53 -0400

    updated to copy files from the data directory

--- packages/caffe_membrane_segmentation/caffe_files/isbi2012/getdata.sh
+++ packages/caffe_membrane_segmentation/caffe_files/isbi2012/getdata.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #
- 
+# The ISBI 2012 data can be downloaded from:
+# 
+#  http://brainiac2.mit.edu/isbi_challenge/home
+#
+# As of this writing, there's also a copy in the
+# this project's data directory.
 
-echo "todo: Will will provide path for wget"
+cp ../../../../data/ISBI2012/*tif .
