ce364425fd77eaa2b6b3c3dcb1ab9dd1a560252c
Pekala <pekalmj1@itsd-loan-ml34.jhuapl.edu>
2015-04-18 13:12:24 -0400

    added

---
+++ packages/caffe_membrane_segmentation/Tests/overlay.m
@@ -0,0 +1,14 @@
+function overlay(baseImage, alphaImage)
+% OVERLAY Quick function for overlaying a probability map on top of a base image.
+%
+%  Here we have in mind as a base image an EM data slice.
+
+imshow(baseImage, 'InitialMag', 'fit');
+green = cat(3, zeros(size(baseImage)), ones(size(baseImage)), zeros(size(baseImage)));
+
+hold on;
+h = imshow(green);
+hold off;
+
+set(h, 'AlphaData', alphaImage);
+
