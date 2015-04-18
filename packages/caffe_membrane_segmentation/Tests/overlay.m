8006728f657eacf5a84196d18d9b59893bb44a21
Mike Pekala <mpekala@umd.edu>
2015-04-18 18:24:45 -0400

    changed API slightly

--- packages/caffe_membrane_segmentation/Tests/overlay.m
+++ packages/caffe_membrane_segmentation/Tests/overlay.m
@@ -1,10 +1,17 @@
-function overlay(baseImage, alphaImage)
+function overlay(baseImage, alphaImage, color)
 % OVERLAY Quick function for overlaying a probability map on top of a base image.
 %
 %  Here we have in mind as a base image an EM data slice.
 
+% default to all green
+if nargin < 3,
+    color = [0,1,0];
+end
+
+r = color(1); g = color(2); b = color(3);
+
 imshow(baseImage, 'InitialMag', 'fit');
-green = cat(3, zeros(size(baseImage)), ones(size(baseImage)), zeros(size(baseImage)));
+green = cat(3, r*ones(size(baseImage)), g*ones(size(baseImage)), b*ones(size(baseImage)));
 
 hold on;
 h = imshow(green);
