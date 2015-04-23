a7e093683f922fc29aeacfbc6635361fd759ae80
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-04-23 10:27:44 -0400

    now supports two overlay layers (makes it easier to show estimates AND truth on top of EM data)

--- packages/caffe_membrane_segmentation/Tests/overlay.m
+++ packages/caffe_membrane_segmentation/Tests/overlay.m
@@ -1,21 +1,29 @@
-function overlay(baseImage, alphaImage, color)
+function overlay(baseImage, overlay1, color1, overlay2, color2)
 % OVERLAY Quick function for overlaying a probability map on top of a base image.
 %
 %  Here we have in mind as a base image an EM data slice.
 
-% default to all green
-if nargin < 3,
-    color = [0,1,0];
+if nargin < 5, color2 = [1,0,0]; end
+if nargin < 4, overlay2 = []; end
+if nargin < 3, color1 = [0,1,0]; end
+
+% render the base image
+imshow(baseImage, 'InitialMag', 'fit');
+hold on;
+
+% overlay 1
+r = color1(1); g = color1(2); b = color1(3);
+layer1 = cat(3, r*ones(size(baseImage)), g*ones(size(baseImage)), b*ones(size(baseImage)));
+h = imshow(layer1);
+set(h, 'AlphaData', double(overlay1));
+
+% overlay 2 (optional)
+if length(overlay2) > 0
+    r = color2(1); g = color2(2); b = color2(3);
+    layer2 = cat(3, r*ones(size(baseImage)), g*ones(size(baseImage)), b*ones(size(baseImage)));
+    h = imshow(layer2);
+    set(h, 'AlphaData', double(overlay2));
 end
 
-r = color(1); g = color(2); b = color(3);
-
-imshow(baseImage, 'InitialMag', 'fit');
-green = cat(3, r*ones(size(baseImage)), g*ones(size(baseImage)), b*ones(size(baseImage)));
-
-hold on;
-h = imshow(green);
 hold off;
-
-set(h, 'AlphaData', double(alphaImage));
 
